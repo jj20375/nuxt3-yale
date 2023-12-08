cf84633aca383ce728f5eb8e7124c06fb9d050f7
Tony柴柴犬 <desiginerice5908@gmail.com>
2023-12-08 13:26:25 +0800

    feature: news 頁面初始化

---
+++ views/template1/News/NewsIndex.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-10">
+        客製化網址{{ route.params.slug }}
+        <div>搜尋參數 id:{{ route.query.id }}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+</script>
