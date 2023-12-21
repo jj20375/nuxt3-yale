9fc4bd71409bace30efbae16a8389eebbbfc84b9
Tony柴柴犬 <desiginerice5908@gmail.com>
2023-12-21 18:46:23 +0800

    refactor: 重構聯絡我們檔案上傳機制

--- views/template1/ContactService/ContactWe/components/ContactWebFileUpload.vue
+++ views/template1/ContactService/ContactWe/components/ContactWebFileUpload.vue
@@ -1,153 +1,139 @@
 <template>
     <div>
         <el-upload
-            v-if="photoFile === null || photoImage !== null"
             ref="upload"
-            :show-file-list="false"
+            :list-type="'picture-card'"
             :on-change="handleChange"
-            :on-remove="handleRemove"
-            :file-list="fileList"
             :auto-upload="false"
-            :limit="1"
             drag
             action=""
+            :limit="5"
             :on-exceed="imageOverLimit"
         >
-            <div class="relative flex items-center justify-center w-full h-full p-5">
-                <div class="w-full flex items-center justify-center border-4 min-h-[228px] border-gray-300 border-dashed">
-                    <!-- 呈現字串得上傳框 -->
-                    <div
-                        v-if="photoFile === null && photoImage === null"
-                        class="YaleSolisW-Rg"
-                    >
-                        <NuxtImg
-                            class="w-[40px] mx-auto"
-                            src="/img/contact/photo-icon.svg"
-                        />
-                        <p class="text-[16px] text-gray-900 mt-[12px]">請點擊上傳照片</p>
-                        <p class="text-gray-700 text-[14px] mt-[4px]">(僅限JPEG、PNG，且不得超過10mb)</p>
-                    </div>
-                    <div
-                        v-if="photoImage !== null"
-                        class="p-2"
-                    >
-                        <img
-                            class="w-full rounded-lg"
-                            :src="photoImage"
-                            alt=""
-                        />
-                    </div>
+            <el-icon><Plus /></el-icon>
+            <template #file="{ file }">
+                <div>
+                    <img
+                        class="el-upload-list__item-thumbnail"
+                        :src="file.url"
+                        alt=""
+                    />
+                    <span class="el-upload-list__item-actions">
+                        <span
+                            class="el-upload-list__item-preview"
+                            @click="handlePictureCardPreview(file)"
+                        >
+                            <el-icon><zoom-in /></el-icon>
+                        </span>
+                        <!-- <span
+                            v-if="!disabled"
+                            class="el-upload-list__item-delete"
+                            @click="handleDownload(file)"
+                        >
+                            <el-icon><Download /></el-icon>
+                        </span> -->
+                        <span
+                            class="el-upload-list__item-delete"
+                            @click="handleRemove(file)"
+                        >
+                            <el-icon><Delete /></el-icon>
+                        </span>
+                    </span>
                 </div>
-            </div>
+            </template>
         </el-upload>
-        <!-- <div class="text-center text-gray-300 el-upload__tip">只能上傳jpg/png文件，且不超過5mb</div> -->
+        <p class="text-gray-700 text-[14px] mt-[4px]">(僅限JPEG、PNG，且不得超過10mb)</p>
+        <el-dialog v-model="showDialog">
+            <img
+                w-full
+                :src="dialogImageUrl"
+                alt="Preview Image"
+            />
+        </el-dialog>
     </div>
 </template>
 
 <script setup lang="ts">
-// element plus 框架
 import { ElMessage } from "element-plus";
-defineOptions({
-    name: "ContactUpload",
-});
-defineExpose({
-    onSubmit,
-});
+import type { UploadProps, UploadFile } from "element-plus";
+const { $utils } = useNuxtApp();
 
-// 上傳圖片檔
-const photoFile = ref(null);
-// 上傳圖片 base64
-const photoImage = ref<any>(null);
-// 檔案列表
-const fileList = ref([]);
-const loading = ref(false);
-
+const fileList = ref<any>(null);
+// 預覽彈窗圖
+const dialogImageUrl = ref("");
+// 顯示預覽彈窗
+const showDialog = ref(false);
 /**
  * 圖片變更
+ * @param file
+ * @param fcFileList
  */
-async function handleChange(file: any) {
-    // 清空上傳檔案列表 以免造成檔案上傳上限超過數量限制
-    fileList.value = [];
-    if (file.size > 5 * 1024 * 1024) {
+function handleChange(file: any, fcFileList: any) {
+    console.log("fcFileList =>", file, fcFileList);
+    fileList.value = fcFileList;
+    if (file.size > 10 * 1024 * 1024) {
         ElMessage({
             type: "error",
-            message: `圖片尺寸不得超過 5MB`,
+            message: `圖片尺寸不得超過 10MB`,
         });
         return;
     }
-    // 設定圖片為上傳檔案
-    photoFile.value = file.raw;
-    photoImage.value = await toBase64(photoFile.value);
-    upLoadPhoto(photoFile.value);
+    // 判斷是否符合圖片類型
+    if (!$utils().isImageType(file.raw.type)) {
+        ElMessage({
+            type: "error",
+            message: "不符合圖片類型(jpg,jpeg,png,gif)",
+        });
+        return;
+    }
 }
-/**
- * 將圖片轉為 base64
- * @param file
- */
-function toBase64(file: any) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-    });
-}
-/**
- * 移除圖片
- */
-function handleRemove() {
-    photoFile.value = null;
-}
+
+const handleRemove: UploadProps["onRemove"] = (removeFile) => {
+    console.log(removeFile);
+    const index = fileList.value.findIndex((item) => item.uid === removeFile.uid);
+    if (index !== -1) {
+        fileList.value.splice(index, 1);
+    }
+};
+
+const handlePictureCardPreview = (file: UploadFile) => {
+    dialogImageUrl.value = file.url!;
+    showDialog.value = true;
+};
+
 /**
  * 上傳圖片超過限制數量
  * @param files
  * @param fileList
  */
 function imageOverLimit(files: any, fileList: any) {
-    console.log(fileList);
     ElMessage({
         type: "error",
         message: "超過上傳圖片上限",
     });
 }
-/**
- * 上傳圖片
- * @param { type Blob or File(Blob 或 檔案格式) } file
- */
-async function upLoadPhoto(file: any) {
-    loading.value = true;
-    emit("update:imageFile", file);
-}
-/**
- * 表單發送
- */
-function onSubmit() {
-    // 判斷是否有上傳存摺照片
-    if (photoImage.value === null) {
-        ElMessage({
-            type: "error",
-            message: "尚未上傳存摺",
-        });
-        return false;
-    }
-    return true;
-}
 </script>
 
 <style lang="scss" scoped>
 :deep .el-upload-dragger {
-    @apply bg-gray-50 rounded-none flex w-full items-center p-0 justify-center border-none;
+    @apply h-full flex items-center justify-center #{!important};
 }
-:deep .el-upload-dragger {
-    @apply h-auto w-full flex-grow overflow-visible;
-}
-:deep .el-upload {
-    @apply w-full;
-}
-:deep .el-dialog {
-    @screen sm {
-        @apply w-[500px];
-    }
-    @apply w-auto;
-}
+// :deep .el-upload-dragger {
+//     @apply flex items-center justify-center border-none;
+// }
+// :deep .el-upload-list {
+//     @apply sr-only;
+// }
+// :deep .el-upload-dragger {
+//     @apply h-auto w-full;
+// }
+// :deep .el-upload {
+//     @apply w-auto;
+// }
+// :deep .el-dialog {
+//     @screen sm {
+//         @apply w-[500px];
+//     }
+//     @apply w-auto;
+// }
 </style>
