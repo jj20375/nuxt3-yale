72613a96c73d7a49863b53da375f1293815c0afa
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-01-04 14:30:51 +0800

    feature: 新贈購物車狀態切換模式
    fix: 購物車商品數量計算金額問題

---
+++ views/template1/ShoppingCar/components/ShoppingCarStep2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex">
+        <div>step2</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// 購物車商品
+import ShoppingCarProducts from "~/views/template1/ShoppingCar/components/ShoppingCarProducts.vue";
+// 折扣優惠
+import ShoppingCarSales from "~/views/template1/ShoppingCar/components/ShoppingCarSales.vue";
+// 輸入優惠券
+import ShoppingCarInputCoupon from "~/views/template1/ShoppingCar/components/ShoppingCarInputCoupon.vue";
+// 訂單金額
+import ShoppingCarBilling from "~/views/template1/ShoppingCar/components/ShoppingCarBilling.vue";
+
+// 購物車選中商品 id
+const selectProductIds = ref<number[]>([]);
+</script>
