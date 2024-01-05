b2c34b2d52996231f23d4f7837275a28dfa90983
aaron.chang@codepulse.com.tw-at-483920983691 <aaron.chang@codepulse.com.tw-at-483920983691>
2024-01-05 10:01:58 +0800

    FQA API改為大寫

--- api/contactService.ts
+++ api/contactService.ts
@@ -6,19 +6,19 @@ export default () => {
         /**
          * 取得問答分類
          */
-        fqaTypeAPI() {
+        FQATypeAPI() {
             return useMyFetch(`${apiUrl}/fqa-category`, { method: "get" });
         },
         /**
          * 取得問答列表
          */
-        fqaListAPI(params: { fqa_category_id: number|string }) {
+        FQAListAPI(params: { fqa_category_id: number|string }) {
             return useMyFetch(`${apiUrl}/fqa`, { method: "get", query: { ...params } });
         },
         /**
          * 取得問答詳情
          */
-        fqaDetailAPI(params: { fqaId: number|string }) {
+        FQADetailAPI(params: { fqaId: number|string }) {
             return useMyFetch(`${apiUrl}/fqa/${params.fqaId}`, { method: "get" });
         },
         /**
