3810e34b0f4f873428c0b15d78a001ebedb991b3
aaron.chang@codepulse.com.tw-at-483920983691 <aaron.chang@codepulse.com.tw-at-483920983691>
2024-01-12 18:11:23 +0800

    串接註冊.登入API

--- views/template1/Auth/Login/LoginIndex.vue
+++ views/template1/Auth/Login/LoginIndex.vue
@@ -14,14 +14,22 @@
                         v-for="(item, index) in formDatas"
                         :key="index"
                     >
-                        <el-form-item :prop="item.prop" :label="item.label">
-                            <el-input :type="item.type" :show-password="item.showPassword"
-                                      :placeholder="item.placeholder" v-model="form[item.prop]"></el-input>
+                        <el-form-item
+                            :prop="item.prop"
+                            :label="item.label"
+                        >
+                            <el-input
+                                :type="item.type"
+                                :show-password="item.showPassword"
+                                :placeholder="item.placeholder"
+                                v-model="form[item.prop]"
+                            ></el-input>
                         </el-form-item>
                     </div>
                     <div class="flex justify-between">
-                        <el-form-item class="!mb-0"
-                                      prop="saveInfo"
+                        <el-form-item
+                            class="!mb-0"
+                            prop="saveInfo"
                         >
                             <el-checkbox
                                 class="!h-fit text-[15px]"
@@ -34,14 +42,14 @@
                     </div>
                     <button
                         class="yellow-btn !py-3 !w-full"
+                        @click.prevent="onSubmit"
                     >
                         登入
                     </button>
                 </div>
             </el-form>
             <div class="relative px-5 mt-[30px]">
-                <div
-                    class="relative divide-text text-center before:absolute before:absolute before:top-1/2 before:left-0 before:h-px before:w-full before:bg-gray-400 before:z-0">
+                <div class="relative divide-text text-center before:absolute before:absolute before:top-1/2 before:left-0 before:h-px before:w-full before:bg-gray-400 before:z-0">
                     <span class="relative px-3 text-gray-400 text-[15px] bg-white z-[2]">使用其他帳號登入</span>
                 </div>
                 <div class="flex gap-[30px] justify-center mt-4">
@@ -60,9 +68,7 @@
                 </div>
                 <div class="flex justify-center gap-3 mt-[30px]">
                     <div class="text-gray-400 text-[15px]">還不是會員?</div>
-                    <NuxtLink
-                        :to="{ name: 'auth-register-slug', params: { slug: '註冊頁' }}"
-                    >
+                    <NuxtLink :to="{ name: 'auth-register-slug', params: { slug: '註冊頁' } }">
                         <div class="text-[15px] underline underline-offset-2 cursor-pointer hover:no-underline">立即註冊</div>
                     </NuxtLink>
                 </div>
@@ -71,12 +77,15 @@
     </section>
 </template>
 <script setup lang="ts">
+import { validateEmail } from "~/service/validator";
+import { ElMessage, ElLoading } from "element-plus";
+const { $api } = useNuxtApp();
 const formRefDom = ref<any>();
 
 const form = ref<any>({
     email: "",
     password: "",
-    saveInfo: false
+    saveInfo: false,
 });
 
 const formDatas = ref<any>([
@@ -84,7 +93,7 @@ const formDatas = ref<any>([
         prop: "email",
         label: "帳號",
         placeholder: "請輸入電子信箱",
-        style: "input"
+        style: "input",
     },
     {
         prop: "password",
@@ -92,8 +101,8 @@ const formDatas = ref<any>([
         placeholder: "請輸入密碼",
         style: "input",
         type: "password",
-        showPassword: true
-    }
+        showPassword: true,
+    },
 ]);
 
 const rules = ref<any>({
@@ -101,15 +110,62 @@ const rules = ref<any>({
         {
             required: true,
             message: "請輸入email",
-            trigger: "blur"
-        }
+            trigger: "blur",
+        },
+        {
+            required: true,
+            validator: validateEmail,
+            trigger: ["change", "blur"],
+            message: "格式不正確",
+        },
     ],
     password: [
         {
             required: true,
             message: "請輸入密碼",
-            trigger: "blur"
-        }
-    ]
+            trigger: "blur",
+        },
+    ],
 });
+
+async function onSubmit() {
+    formRefDom.value.validate(async (valid: any) => {
+        if (!valid) {
+            ElMessage({
+                type: "error",
+                message: `尚有欄位未填`,
+            });
+        } else {
+            const loading = ElLoading.service({
+                lock: true,
+                text: "登入中...",
+                background: "rgba(0, 0, 0, 0.7)",
+            });
+            try {
+                const params = {
+                    password: form.value.password,
+                    email: form.value.email,
+                };
+                const { data, status, error } = await $api().LoginAPI(params);
+                console.log(data, status.value, error);
+                if (status.value === 'success') {
+                    ElMessage({
+                        type: "success",
+                        message: `登入成功`,
+                    });
+                } else {
+                    ElMessage({
+                        type: "error",
+                        message: (error.value as any).data.message,
+                    });
+                }
+                
+                loading.close();
+            } catch (err) {
+                loading.close();
+                console.log("HomeSampleAPI => ", err);
+            }
+        }
+    });
+}
 </script>
