6765011ebe261398bae76d9c372d82a71259b792
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-01-15 15:05:49 +0800

    feature: 新增 yale home app 路由

---
+++ views/template1/YaleHomeApp/YaleHomeAppIndex.vue
@@ -0,0 +1,30 @@
+<template>
+    <BannerLayout
+        :title="'Yale Home App'"
+        :banner="'/img/yale-home-app/yale-home-app-banner.jpg'"
+    >
+        <template #breadcrumbs>
+            <Breadcrumb :menus="breadcrumbs" />
+        </template>
+
+        <template #content> </template>
+    </BannerLayout>
+</template>
+
+<script lang="ts" setup>
+import BannerLayout from "~/views/template1/layouts/BannerLayout.vue";
+import Breadcrumb from "~/views/template1/components/Breadcrumb.vue";
+const route = useRoute();
+
+const breadcrumbs = ref([
+    {
+        name: "index",
+        text: "首頁",
+    },
+    {
+        name: "faq-slug",
+        text: "Yale Home App",
+        params: { slug: "Yale Home APP" },
+    },
+]);
+</script>
