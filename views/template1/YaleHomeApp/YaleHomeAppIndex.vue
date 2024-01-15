e249475948f501195b40f4f41806e81738e65606
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-01-15 17:30:05 +0800

    feature: yale home app 切版

--- views/template1/YaleHomeApp/YaleHomeAppIndex.vue
+++ views/template1/YaleHomeApp/YaleHomeAppIndex.vue
@@ -7,13 +7,29 @@
             <Breadcrumb :menus="breadcrumbs" />
         </template>
 
-        <template #content> </template>
+        <template #content>
+            <div class="container mt-[60px]">
+                <YaleHomeAppTop :datas="topDatas" />
+            </div>
+            <YaleHomeAppCarousel :photos="carouselDatas" />
+            <YaleHomeAppProduct />
+            <YaleHomeAppProductFeature />
+        </template>
     </BannerLayout>
 </template>
 
 <script lang="ts" setup>
 import BannerLayout from "~/views/template1/layouts/BannerLayout.vue";
 import Breadcrumb from "~/views/template1/components/Breadcrumb.vue";
+// 上方區塊
+import YaleHomeAppTop from "~/views/template1/YaleHomeApp/components/YaleHomeAppTop.vue";
+// 幻燈片區塊
+import YaleHomeAppCarousel from "~/views/template1/YaleHomeApp/components/YaleHomeAppCarousel.vue";
+// Yale home app 介紹
+import YaleHomeAppProduct from "~/views/template1/YaleHomeApp/components/YaleHomeAppProduct.vue";
+// Yale home app 功能
+import YaleHomeAppProductFeature from "~/views/template1/YaleHomeApp/components/YaleHomeAppProductFeature.vue";
+
 const route = useRoute();
 
 const breadcrumbs = ref([
@@ -25,6 +41,33 @@ const breadcrumbs = ref([
         name: "yale-home-app-slug",
         text: "Yale Home App",
         params: { slug: "Yale Home APP" },
+    },
+]);
+
+// 上方區塊資料
+const topDatas = ref([]);
+
+for (let i = 1; i <= 2; i++) {
+    topDatas.value.push({
+        imgSrc: "/img/yale-home-app/demo/yale-home-app-top-1.jpg",
+        title: "測試標題-" + i,
+        content: "Yale Home是Yale耶魯提供住宅市場的全球智慧門鎖解決方案，Yale智慧鎖可以透過Yale Home應用程式進行管理，讓您完全控制您的門鎖，亦可藉由您的手機裝置上鎖或解鎖您的大門、允許他人進入，並記錄訪客使用情況。",
+    });
+}
+
+// 幻燈片區塊資料
+const carouselDatas = ref([
+    {
+        imgSrc: "/img/yale-home-app/demo/yale-home-app-carousel-1.jpg",
+    },
+    {
+        imgSrc: "/img/yale-home-app/demo/yale-home-app-carousel-2.jpg",
+    },
+    {
+        imgSrc: "/img/yale-home-app/demo/yale-home-app-carousel-3.jpg",
+    },
+    {
+        imgSrc: "/img/yale-home-app/demo/yale-home-app-carousel-1.jpg",
     },
 ]);
 </script>
---
+++ views/template1/YaleHomeApp/components/YaleHomeAppTop.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <div
+            class="mb-[30px]"
+            v-for="(data, index) in datas"
+        >
+            <NuxtImg
+                class="w-full"
+                :src="data.imgSrc"
+            />
+            <h3 class="text-[32px] font-medium YaleSolisW-Bd mt-[30px] text-gray-800">{{ data.title }}</h3>
+            <p class="text-[16px] text-gray-800 mt-[20px]">{{ data.content }}</p>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    datas: {
+        imgSrc: string;
+        title: string;
+        content: string;
+    }[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    datas: [{ imgSrc: "", title: "", content: "" }],
+});
+</script>
