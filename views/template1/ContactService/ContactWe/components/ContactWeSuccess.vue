5a82aa069fbd1a14f15d1b6ed32c39ea41041f10
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-02-01 18:03:26 +0800

    feature: 新增聯絡我們送出表單成功頁
    fix: 保固登記｜線上報修｜新光保全安裝 表單送出成功後 點擊繼續按鈕改回到首頁

--- views/template1/ContactService/ContactWe/components/ContactWeSuccess.vue
+++ views/template1/ContactService/ContactWe/components/ContactWeSuccess.vue
@@ -7,8 +7,8 @@
                     :height="100"
                     :width="100"
                 />
-                <div class="font-bold text-[28px] text-center mt-5 mb-5">線上報修申請已送出</div>
-                <div class="text-center">您已完成線上報修申請。</div>
+                <div class="font-bold text-[28px] text-center mt-5 mb-5">聯絡我們表單已送出</div>
+                <div class="text-center">感謝您的訊息，我們將會在收到信後盡速與您聯繫。</div>
                 <div class="flex justify-center mt-10">
                     <NuxtLink :to="{ name: 'index' }">
                         <button class="yellow-btn">繼續</button>
--- views/template1/ContactService/Reservation/ReservationSuccess.vue
+++ views/template1/ContactService/Reservation/ReservationSuccess.vue
@@ -10,7 +10,7 @@
                 <div class="font-bold text-[28px] text-center mt-5 mb-5">安裝預約申請已送出</div>
                 <div class="text-center">感謝您的訊息，我們將會在收到信後盡速與您聯繫。</div>
                 <div class="flex justify-center mt-10">
-                    <NuxtLink :to="{ name: 'reservation-slug', params: { slug: '預約安裝' } }">
+                    <NuxtLink :to="{ name: 'index' }">
                         <button class="yellow-btn">繼續</button>
                     </NuxtLink>
                 </div>
