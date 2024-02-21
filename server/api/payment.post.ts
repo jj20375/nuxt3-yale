8332f26e249ee136e999125b8814d09b64888e36
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-02-21 17:15:13 +0800

    feature: 新增接收金流 api callback 回應值

--- server/api/payment.post.ts
+++ server/api/payment.post.ts
@@ -2,5 +2,5 @@ export default defineEventHandler(async (event) => {
     // handle POST requests for the `api/foo` endpoint
     const body = await readBody(event);
     // await sendRedirect(event, "/", 301);
-    return { ...body };
+    return { ...body, message: "ok" };
 });
