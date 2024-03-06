e68879d1c1b90ccd188c78b2b9bde5c91906c314
Tony柴柴犬 <desiginerice5908@gmail.com>
2024-03-07 01:39:52 +0800

    feature: 新增金流付款導頁機制

--- views/template1/Order/OrderIndex.vue
+++ views/template1/Order/OrderIndex.vue
@@ -1,44 +1,20 @@
 <template>
-    <div class="sm:flex justify-center mt-[40px]">
-        <div class="flex justify-center mt-[60px]">
-            <div>
-                <Vue3Lottie
-                    animationLink="/json/check.json"
-                    :height="100"
-                    :width="100"
-                />
-                <h1 class="font-bold text-[28px] text-center mt-5 mb-5">訂單建立成功</h1>
-                <p class="text-center mb-[40px] text-gray-800 text-[16px]">
-                    您的訂單已經成功送出，我們會在第一時間進行處理！ <br />
-                    請至會員中心-我的訂單查詢，或於結帳時所填之 Email 信箱檢視郵件以追蹤訂單狀態。<br />
-                    假如您有任何疑問，請撥打服務專線 {{ contactPhone }}，感謝您的訂購。
-                </p>
-                <div class="flex justify-center">
-                    <button
-                        @click.prevent="goToOrderList"
-                        class="yellow-btn !px-16"
-                    >
-                        查看訂單狀態
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <component :is="showComponent"></component>
 </template>
 
 <script setup lang="ts">
 import { Vue3Lottie } from "vue3-lottie";
+import OrderSuccess from "~/views/template1/Order/components/OrderSuccess.vue";
+import OrderFail from "~/views/template1/Order/components/OrderFail.vue";
 
 const router = useRouter();
 const route = useRoute();
 
-const contactPhone = ref("0912345678");
-
-function goToOrderList() {
-    if (route.query.type === "type1") {
-        router.push({ name: "auth-order-slug", params: { slug: "訂單記錄" } });
+const showComponent = computed(() => {
+    if (route.query.status === "error") {
+        return OrderFail;
     } else {
-        router.push({ name: "auth-door-slug", params: { slug: "訂製門扇-訂單記錄" } });
+        return OrderSuccess;
     }
-}
+});
 </script>
