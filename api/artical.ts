23c0565d95e5d7d1b38d16618f0daf4761478e5a
aaron.chang@codepulse.com.tw-at-483920983691 <aaron.chang@codepulse.com.tw-at-483920983691>
2024-04-23 13:10:06 +0800

    消息排序

--- api/artical.ts
+++ api/artical.ts
@@ -7,7 +7,7 @@ export default () => {
          * 取得首頁裝修實績分類
          */
         ArticalTypeAPI(params: { type: string }) {
-            return useMyFetch(`${apiUrl}/article-category`, { method: "get", query: { ...params } });
+            return useMyFetch(`${apiUrl}/article-category`, { method: "get", query: { order_by: 'sort_order', order_direction: 'asc', ...params } });
         },
         /**
          * 取得文章分類詳情
@@ -19,13 +19,13 @@ export default () => {
          * 取得裝修實績列表
          */
         ArticalListAPI(params: { per_page: number; page: number; article_category_id: number | string; search_relations?: string }) {
-            return useMyFetch(`${apiUrl}/article/paginate`, { method: "get", query: { ...params } });
+            return useMyFetch(`${apiUrl}/article/paginate`, { method: "get", query: { order_by: 'sort', order_direction: 'asc', ...params } });
         },
         /**
          * 取得文章詳情
          */
         SampleDetailAPI(params: { articleId: number | string }) {
-            return useMyFetch(`${apiUrl}/article/${params.articleId}`, { method: "get" });
+            return useMyFetch(`${apiUrl}/article/${params.articleId}`, { method: "get", query: { order_by: 'sort', order_direction: 'asc' } } );
         },
     };
 };
